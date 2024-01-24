7523ccc69d433ca1648140f11e0e76d43f455ea1
Mateusz <alirymc@gmail.com>
2024-01-24 03:59:41 +0100

    repair same mist

--- src/app/components/contact.js
+++ src/app/components/contact.js
@@ -16,7 +16,7 @@ export default function Contact(){
     const senData =async()=>{
       setformdata({type,message})
       try{
-        const response = await fetch('http://localhost:3000/api/send',({
+        const response = await fetch('https://personalweb-navy.vercel.app/api/send',({
           method:'POST',
           headers:{
             'Content-Type':'application/json'

499639a04c46c9ad2cbfa62f107a75baa7c094bb
Mateusz <alirymc@gmail.com>
2024-01-24 22:46:33 +0100

    Add suer send data status button

--- src/app/components/contact.js
+++ src/app/components/contact.js
@@ -1,4 +1,5 @@
 import styles from "../page.module.css"
+import stylebutton from "./constact.module.css"
 import { useState } from "react"
 import gsap from "gsap"
 import { useGSAP } from "@gsap/react"
@@ -9,14 +10,16 @@ export default function Contact(){
     const [formdata,setformdata]=useState({})
     const [telefon,setTelefon] = useState(1) 
     const [response,setresponse] = useState()
+    const [pending, setpending] = useState(false)
+    const [status,setStatus]=useState()
     useGSAP(()=>{
         const lo = gsap.timeline({repeat:-1,repeatDelay:2})
         lo.fromTo(".page_underor__PsW_i",{width:0,backgroundColor:'#6B604F'},{width:'100%',backgroundColor:'transparent',duration:1})
     })
     const senData =async()=>{
-      setformdata({type,message})
       try{
-        const response = await fetch('https://personalweb-navy.vercel.app/api/send',({
+        setpending(true)
+        const response = await fetch('https://mateuszwebsite.pl/api/send',({
           method:'POST',
           headers:{
             'Content-Type':'application/json'
@@ -26,10 +29,16 @@ export default function Contact(){
             message:message.message
           })
         }))
+
         if(response.ok){
           const data  =await response.json()
           console.log(`${data.message}`)
-          if(data.message==="SUCCESS"){setresponse(true),clear()}
+          if(data.message==="SUCCESS"){setresponse(true),clear(),setpending(false),setStatus("Message sent correct")}else{
+            setStatus("Bad data or not all fild fill")
+            setresponse(false)
+            clear()
+            setpending(false)
+          }
         }else{
           console.log("Samething went wrong")
         }
@@ -61,7 +70,8 @@ export default function Contact(){
             <span className={styles.underor}></span>
             <span className={styles.spanContact} onClick={()=>{telefon?setTelefon(0):setTelefon(1)}}>{telefon?"or use email":"or use phone"}</span>
           </div>
-          <button className={styles.buttonContact} onClick={senData} style={response?{pointerEvents:'none'}:{pointerEvents:'all'}}>{response?"Sent":"Send"}</button>
+          <span className={stylebutton.spanerror}>{status?status:null}</span>
+          {pending?<div id={stylebutton.pending}><span id={stylebutton.dot}></span><span id={stylebutton.dotleft}></span><span id={stylebutton.dotright}></span></div>:<button className={styles.buttonContact} onClick={senData} style={response?{pointerEvents:'none'}:{pointerEvents:'all'}}>{response?"Sent":"Send"}</button>}
         </div>
       </div>
     )
